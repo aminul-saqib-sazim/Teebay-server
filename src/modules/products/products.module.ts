ba6a77cb8195f27ad48741523343e9bd989ee2d9
Saqib <aminul.saqib@sazim.io>
2026-01-21 12:44:05 +0600

    feat(TB-19): added rent product feature
    feat(TB-19): rent product

--- src/modules/products/products.module.ts
+++ src/modules/products/products.module.ts
@@ -3,14 +3,15 @@ import { Module } from "@nestjs/common";
 import { MikroOrmModule } from "@mikro-orm/nestjs";
 
 import { Product } from "@/common/entities/products.entity";
+import { OrdersModule } from "@/modules/orders/orders.module";
 
 import { ProductsController } from "./products.controller";
 import { ProductsService } from "./products.service";
 
 @Module({
-  imports: [MikroOrmModule.forFeature([Product])],
+  imports: [MikroOrmModule.forFeature([Product]), OrdersModule],
   controllers: [ProductsController],
   providers: [ProductsService],
   exports: [ProductsService],
 })
-export class ProductsModule { }
+export class ProductsModule {}
